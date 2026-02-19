3a9961be19e58a1bd7b4eba940b3778ed345adae
pacoa1 <a01706242@tec.mx>
2026-02-19 10:16:05 -0600

    imprimir números en orden con funciones asincrónicas

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ console.log ("hola desde node!");
 const fileSystem = require("fs");
 
 fileSystem.writeFileSync("hola.txt", "Hola desde node!");
+
+setTimeout(() => {
+    console.log("jojojojo te hackie, arriba las chivaaaas!!!!!");
+}, 15000);
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
+
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
