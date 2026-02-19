fa3cfb013cddfe265dfe13ecf94a5ce5e5d4e8b0
pacoa1 <a01706242@tec.mx>
2026-02-19 10:33:35 -0600

    Crear servidor y enviar respuesta http con node

--- app.js
+++ app.js
@@ -15,3 +15,12 @@ for (let item of arreglo) {
         console.log(item);
     }, item);
 }
+
+const http = require("http");
+
+const server = http.createServer((request, response) => {
+    console.log(request.url);
+    response.end();
+});
+
+server.listen(3000);
