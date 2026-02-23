f9d59549ec05273f33b04d3708f26a0cbc13a818
pacoa1 <a01706242@tec.mx>
2026-02-23 09:47:50 -0600

    clean up para lab 10

--- app.js
+++ app.js
@@ -1,20 +1,4 @@
-console.log ("hola desde node!");
-
-const fileSystem = require("fs");
-
-fileSystem.writeFileSync("hola.txt", "Hola desde node!");
-
-setTimeout(() => {
-    console.log("jojojojo te hackie, arriba las chivaaaas!!!!!");
-}, 15000);
-
-const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
-
-for (let item of arreglo) {
-    setTimeout(() => {
-        console.log(item);
-    }, item);
-}
+const http = require("http");
 
 const html = `
 <!DOCTYPE html>
@@ -85,8 +69,6 @@ const html = `
   </body>
 </html>
 `;
-
-const http = require("http");
 
 const server = http.createServer((request, response) => {
     console.log(request.url);
