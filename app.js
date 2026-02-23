f249b3503a5aaf70eafb535765f084e31371212a
pacoa1 <a01706242@tec.mx>
2026-02-23 10:48:58 -0600

    generar html dinámico a partir de los datos de un arreglo

--- app.js
+++ app.js
@@ -28,55 +28,6 @@ const html_footer = `
   </html>  
 `;
 
-const html_index = `
-  <a href="/new"><button class="button is-primary">Nuevo videojuego</button></a>
-    <div class="columns">
-      <div class="column">
-          <div id="halo"></div>
-      </div>
-      <div class="column">
-          Minecraft
-          <figure class="image">
-              <img class="is-rounded" src="https://store-images.s-microsoft.com/image/apps.58378.13850085746326678.826cc014-d610-46af-bdb3-c5c96be4d22c.64287a91-c69e-4723-bb61-03fecd348c2a?q=90&w=480&h=270" />
-          </figure>
-      </div>    
-      <div class="column">      
-                  <li>Cyberpunk</li>
-                  <li>Doom</li>
-                  <li>Gears of war</li>
-      </div>
-    </div>
-  </div>
-</section>
-<section class="section">
-  <div class="container">
-    <div class="columns">
-      <div class="column">
-        <h1 class="title">Comandos de git</h1>
-        <ul>
-          <li>git add: Sirve para agregar cambios a la transacción.</li>
-          <li>
-            git commit -m "mensaje en imperativo": Sirve para comprometer 
-            la transacción, es decir, guardar los cambios.
-          </li>
-          <li>git checkout <strong>[nombre_rama]</strong>: Sirve para cambiarse de rama.</li>
-          <li>
-            git checkout -b [nombre_rama]: Sirve para crear una nueva rama y 
-            cambiarse a esa nueva rama.
-          </li>
-          <li>
-            git push: Sirve para sincronizar los cambios desde mi repositorio 
-            hacia el repositorio remoto.
-          </li>
-          <li>
-            git pull: Sirve para sincronizar los cambios del repositorio remoto 
-            hacia mi repositorio.
-          </li>
-        </ul>
-      </div>
-    </div>  
-`;
-
 const html_form = `
 <form action="/new" method="POST">
   <div class="field">
@@ -97,10 +48,67 @@ const html_form = `
 </form>
 `;
 
+const videojuegos = [
+  {
+    nombre: "Minecraft",
+    imagen: "https://store-images.s-microsoft.com/image/apps.58378.13850085746326678.826cc014-d610-46af-bdb3-c5c96be4d22c.64287a91-c69e-4723-bb61-03fecd348c2a?q=90&w=480&h=270"
+  },
+  {
+    nombre: "Gears of war",
+    imagen: "https://upload.wikimedia.org/wikipedia/en/thumb/8/82/Gears_of_war_cover_art.jpg/250px-Gears_of_war_cover_art.jpg"
+  },
+]
+
 const server = http.createServer((request, response) => {
 
      if (request.url == "/") {
         response.setHeader('Content-Type', 'text/html');
+         let html_index = `
+              <a href="/new"><button class="button is-primary">Nuevo videojuego</button></a>
+              <div class="columns">`;
+
+        for (let juego of videojuegos) {
+            html_index += `
+                <div class="column">
+                    ${juego.nombre}
+                    <figure class="image">
+                        <img class="is-rounded" src="${juego.imagen}" />
+                    </figure>
+                </div>`;
+        }
+        
+        html_index += `    
+              </div>
+            </div>
+          </section>
+          <section class="section">
+            <div class="container">
+              <div class="columns">
+                <div class="column">
+                  <h1 class="title">Comandos de git</h1>
+                  <ul>
+                    <li>git add: Sirve para agregar cambios a la transacción.</li>
+                    <li>
+                      git commit -m "mensaje en imperativo": Sirve para comprometer 
+                      la transacción, es decir, guardar los cambios.
+                    </li>
+                    <li>git checkout <strong>[nombre_rama]</strong>: Sirve para cambiarse de rama.</li>
+                    <li>
+                      git checkout -b [nombre_rama]: Sirve para crear una nueva rama y 
+                      cambiarse a esa nueva rama.
+                    </li>
+                    <li>
+                      git push: Sirve para sincronizar los cambios desde mi repositorio 
+                      hacia el repositorio remoto.
+                    </li>
+                    <li>
+                      git pull: Sirve para sincronizar los cambios del repositorio remoto 
+                      hacia mi repositorio.
+                    </li>
+                  </ul>
+                </div>
+              </div>  
+          `;
         response.write(html_header + html_index + html_footer);
         response.end();
     } else if (request.url == "/new" && request.method == "GET") {
