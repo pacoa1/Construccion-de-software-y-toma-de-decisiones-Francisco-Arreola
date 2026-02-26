318badb97da5dc45dad8533575e3ef8dafdf07f1
pacoa1 <a01706242@tec.mx>
2026-02-26 09:47:55 -0600

    enviar error 404

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ app.use(bodyParser.urlencoded({extended: false}));
 const rutasVideojuegos = require('./routes/videojuegos.routes');
 app.use('/videojuegos', rutasVideojuegos);
 
+app.use ((request, response, next) => {
+    response.status(404).send('El videojuego no existe :(');
+});
+
 app.listen(3000);
