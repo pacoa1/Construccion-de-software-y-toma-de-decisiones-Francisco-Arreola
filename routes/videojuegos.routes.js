5ada7109304d336b4f9d852ec74ef70aacb11f8f
pacoa1 <a01706242@tec.mx>
2026-03-02 09:55:06 -0600

    asignar varias rutas para el mismo controlador

--- routes/videojuegos.routes.js
+++ routes/videojuegos.routes.js
@@ -4,8 +4,13 @@ const router = express.Router();
 const videojuegosController = require('../controllers/videojuegos.controller');
 
 router.get('/new', videojuegosController.get_new);
+router.get('/nuevo', videojuegosController.get_new);
+router.get('/add', videojuegosController.get_new);
 router.post('/new', videojuegosController.post_new);
+router.post('/nuevo', videojuegosController.post_new);
+router.post('/add', videojuegosController.get_new);
 router.get('/old', videojuegosController.get_old);
+router.get('/old_labs', videojuegosController.get_old);
 router.use(videojuegosController.get_list);
 
 module.exports = router;
