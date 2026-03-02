2d44a4870dc7b50d8559adb5b6adcbe39622a285
pacoa1 <a01706242@tec.mx>
2026-03-02 09:54:05 -0600

    crear acciones del controlador

--- routes/videojuegos.routes.js
+++ routes/videojuegos.routes.js
@@ -3,32 +3,9 @@ const router = express.Router();
 
 const videojuegosController = require('../controllers/videojuegos.controller');
 
-const path = require('path');
-
-const videojuegos = [
-  {
-    nombre: "Minecraft",
-    imagen: "https://store-images.s-microsoft.com/image/apps.58378.13850085746326678.826cc014-d610-46af-bdb3-c5c96be4d22c.64287a91-c69e-4723-bb61-03fecd348c2a?q=90&w=480&h=270"
-  },
-  {
-    nombre: "Gears of war",
-    imagen: "https://upload.wikimedia.org/wikipedia/en/thumb/8/82/Gears_of_war_cover_art.jpg/250px-Gears_of_war_cover_art.jpg"
-  },
-];
-
 router.get('/new', videojuegosController.get_new);
-
-router.post('/new', (request, response, next) => {
-    videojuegos.push(request.body);
-    response.redirect('/videojuegos');
-});
-
-router.get ('/old', (request, response, next) => {
-    response.sendFile(path.join(__dirname, '..' , 'old_labs' , 'js' , 'index.html'));
-});
- 
-router.use((request, response, next) => {
-    response.render('list', {videojuegos: videojuegos}); //Manda la respuesta
-});
+router.post('/new', videojuegosController.post_new);
+router.get('/old', videojuegosController.get_old);
+router.use(videojuegosController.get_list);
 
 module.exports = router;
