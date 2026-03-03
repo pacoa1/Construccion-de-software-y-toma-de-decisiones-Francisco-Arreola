42940bb600677d01f9884f0a47dca3bb339bafe4
pacoa1 <a01706242@tec.mx>
2026-03-03 10:26:54 -0600

    recuperar variable de sesión

--- controllers/videojuegos.controller.js
+++ controllers/videojuegos.controller.js
@@ -3,7 +3,9 @@ const Videojuego = require('../models/videojuego.model');
 const path = require('path');
 
 exports.get_new = (request, response, next) => {
-    response.render('new');
+      response.render('new', {
+        username: request.session.username || '',
+    });
 };
 
 exports.post_new = (request, response, next) => {
@@ -19,5 +21,8 @@ exports.get_old = (request, response, next) => {
 
 exports.get_list = (request, response, next) => {
     console.log(request.get('Cookie'));
-    response.render('list', {videojuegos: Videojuego.fetchAll()}); //Manda la respuesta
+    response.render('list', {
+        username: request.session.username || '',
+        videojuegos: Videojuego.fetchAll(),
+    }); //Manda la respuesta
 };
