d398bf86dc0f73b81e7b4d0826578914d1536b4a
pacoa1 <a01706242@tec.mx>
2026-03-10 10:11:10 -0600

    crear middleware para privilegio ver_videojuegos

--- routes/videojuegos.routes.js
+++ routes/videojuegos.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const isAuth = require('../util/is-auth');
+const canView = require('../util/can-view');
 const videojuegosController = require('../controllers/videojuegos.controller');
 
 router.get('/new', isAuth, videojuegosController.get_new);
@@ -12,7 +13,7 @@ router.post('/nuevo', isAuth, videojuegosController.post_new);
 router.post('/add', isAuth, videojuegosController.get_new);
 router.get('/old', isAuth, videojuegosController.get_old);
 router.get('/old_labs', isAuth, videojuegosController.get_old);
-router.get('/:videojuego_id', isAuth, videojuegosController.get_list);
-router.use(isAuth, videojuegosController.get_list);
+router.get('/:videojuego_id', isAuth, canView, videojuegosController.get_list);
+router.use(isAuth, canView, videojuegosController.get_list);
 
 module.exports = router;
