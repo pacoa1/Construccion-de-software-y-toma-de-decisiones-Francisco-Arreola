ca22c99bf9b475c60440a5f8cc91948fe172f66c
pacoa1 <a01706242@tec.mx>
2026-03-10 10:27:30 -0600

    actualizar sql para soportar rbac

--- sql/videojuegos.sql
+++ sql/videojuegos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 05, 2026 at 04:37 PM
+-- Generation Time: Mar 10, 2026 at 04:41 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,6 +24,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `otorga`
+--
+
+CREATE TABLE `otorga` (
+  `id_rol` int(11) NOT NULL,
+  `id_privilegio` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `privilegios`
+--
+
+CREATE TABLE `privilegios` (
+  `id` int(11) NOT NULL,
+  `privilegio` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `rol` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tiene`
+--
+
+CREATE TABLE `tiene` (
+  `id_usuario` varchar(50) NOT NULL,
+  `id_rol` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -33,6 +81,15 @@ CREATE TABLE `users` (
   `nombre` varchar(300) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `nombre`, `created_at`) VALUES
+('acantu', '$2b$12$rXegq4954JwPGwLuJ5uiR.K/Y/JtwX0pXJR7y/HtzqBFutGJgXp9W', 'Andrea Cantú', '2026-03-09 15:54:58'),
+('citibanamex', '$2b$12$0TO6GxusFKTEwZ4cGqnd5.RWLI.2UK5J3ezfChZ0Rfnqzqg4NCLA.', 'Alexis Berthou', '2026-03-09 15:58:01'),
+('lalo3', '$2b$12$9BaWo0uW13UhhzR0qLpjQuR0eE6YojdHLsLGFfjPrGVTWdbIyUp9y', 'asdfg', '2026-03-09 16:38:39');
 
 -- --------------------------------------------------------
 
@@ -48,8 +105,43 @@ CREATE TABLE `videojuegos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
+-- Dumping data for table `videojuegos`
+--
+
+INSERT INTO `videojuegos` (`id`, `nombre`, `imagen`, `created_at`) VALUES
+(1, 'Minecraft', 'https://store-images.s-microsoft.com/image/apps.58378.13850085746326678.826cc014-d610-46af-bdb3-c5c96be4d22c.64287a91-c69e-4723-bb61-03fecd348c2a?q=90&w=480&h=270', '2026-03-05 16:01:52'),
+(2, 'Gears of war', 'https://upload.wikimedia.org/wikipedia/en/thumb/8/82/Gears_of_war_cover_art.jpg/250px-Gears_of_war_cover_art.jpg', '2026-03-05 16:01:52'),
+(3, '\'); Select * from videojuegos;--', 'https://upload.wikimedia.org/wikipedia/en/5/52/Assassin%27s_Creed.jpg', '2026-03-05 16:27:53');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `otorga`
+--
+ALTER TABLE `otorga`
+  ADD PRIMARY KEY (`id_rol`,`id_privilegio`),
+  ADD KEY `id_privilegio` (`id_privilegio`);
+
+--
+-- Indexes for table `privilegios`
+--
+ALTER TABLE `privilegios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tiene`
+--
+ALTER TABLE `tiene`
+  ADD PRIMARY KEY (`id_rol`,`id_usuario`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indexes for table `users`
@@ -68,10 +160,40 @@ ALTER TABLE `videojuegos`
 --
 
 --
+-- AUTO_INCREMENT for table `privilegios`
+--
+ALTER TABLE `privilegios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `videojuegos`
 --
 ALTER TABLE `videojuegos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `otorga`
+--
+ALTER TABLE `otorga`
+  ADD CONSTRAINT `otorga_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`),
+  ADD CONSTRAINT `otorga_ibfk_2` FOREIGN KEY (`id_privilegio`) REFERENCES `privilegios` (`id`);
+
+--
+-- Constraints for table `tiene`
+--
+ALTER TABLE `tiene`
+  ADD CONSTRAINT `tiene_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`username`),
+  ADD CONSTRAINT `tiene_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
