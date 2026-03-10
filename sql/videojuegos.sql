3038a71d17538c6d87e54ef57cd1d9cddeeff5a0
pacoa1 <a01706242@tec.mx>
2026-03-10 11:49:37 -0600

    Actalizar Script SQL

--- sql/videojuegos.sql
+++ sql/videojuegos.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 10, 2026 at 04:41 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Servidor: localhost
+-- Tiempo de generación: 10-03-2026 a las 18:48:08
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `videojuegos2`
+-- Base de datos: `videojuegos2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `otorga`
+-- Estructura de tabla para la tabla `otorga`
 --
 
 CREATE TABLE `otorga` (
@@ -34,7 +34,7 @@ CREATE TABLE `otorga` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Dumping data for table `otorga`
+-- Volcado de datos para la tabla `otorga`
 --
 
 INSERT INTO `otorga` (`id_rol`, `id_privilegio`, `created_at`) VALUES
@@ -46,7 +46,7 @@ INSERT INTO `otorga` (`id_rol`, `id_privilegio`, `created_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `privilegios`
+-- Estructura de tabla para la tabla `privilegios`
 --
 
 CREATE TABLE `privilegios` (
@@ -56,7 +56,7 @@ CREATE TABLE `privilegios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Dumping data for table `privilegios`
+-- Volcado de datos para la tabla `privilegios`
 --
 
 INSERT INTO `privilegios` (`id`, `privilegio`, `created_at`) VALUES
@@ -67,7 +67,7 @@ INSERT INTO `privilegios` (`id`, `privilegio`, `created_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `roles`
+-- Estructura de tabla para la tabla `roles`
 --
 
 CREATE TABLE `roles` (
@@ -77,7 +77,7 @@ CREATE TABLE `roles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Dumping data for table `roles`
+-- Volcado de datos para la tabla `roles`
 --
 
 INSERT INTO `roles` (`id`, `rol`, `created_at`) VALUES
@@ -87,7 +87,7 @@ INSERT INTO `roles` (`id`, `rol`, `created_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tiene`
+-- Estructura de tabla para la tabla `tiene`
 --
 
 CREATE TABLE `tiene` (
@@ -97,18 +97,18 @@ CREATE TABLE `tiene` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Dumping data for table `tiene`
+-- Volcado de datos para la tabla `tiene`
 --
 
 INSERT INTO `tiene` (`id_usuario`, `id_rol`, `created_at`) VALUES
-('citibanamex', 1, '2026-03-10 15:46:32'),
-('acantu', 2, '2026-03-10 15:46:32'),
-('sebas', 2, '2026-03-10 16:30:27');
+('Paco14', 1, '2026-03-10 15:46:32'),
+('CH14', 2, '2026-03-10 15:46:32'),
+('HormigaOtaku', 2, '2026-03-10 16:30:27');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tipo`
+-- Estructura de tabla para la tabla `tipo`
 --
 
 CREATE TABLE `tipo` (
@@ -118,7 +118,7 @@ CREATE TABLE `tipo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Dumping data for table `tipo`
+-- Volcado de datos para la tabla `tipo`
 --
 
 INSERT INTO `tipo` (`id`, `nombre`, `created_at`) VALUES
@@ -130,7 +130,7 @@ INSERT INTO `tipo` (`id`, `nombre`, `created_at`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Estructura de tabla para la tabla `users`
 --
 
 CREATE TABLE `users` (
@@ -141,19 +141,18 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Dumping data for table `users`
+-- Volcado de datos para la tabla `users`
 --
 
 INSERT INTO `users` (`username`, `password`, `nombre`, `created_at`) VALUES
-('acantu', '$2b$12$rXegq4954JwPGwLuJ5uiR.K/Y/JtwX0pXJR7y/HtzqBFutGJgXp9W', 'Andrea Cantú', '2026-03-09 15:54:58'),
-('citibanamex', '$2b$12$0TO6GxusFKTEwZ4cGqnd5.RWLI.2UK5J3ezfChZ0Rfnqzqg4NCLA.', 'Alexis Berthou', '2026-03-09 15:58:01'),
-('lalo3', '$2b$12$9BaWo0uW13UhhzR0qLpjQuR0eE6YojdHLsLGFfjPrGVTWdbIyUp9y', 'asdfg', '2026-03-09 16:38:39'),
-('sebas', '$2b$12$V1BGWjk1TWtbMON46n2kkOyamsST//NqHTFBh9YX/ny7me6ZRBK2i', 'Sebastián Mansilla', '2026-03-10 16:06:55');
+('CH14', '$2b$12$H.GTxppWbI2wxaKf5kf/6ebeYJlWtRlvxCxrOeGpAClh5ov8Ea31y', 'Javier Hernandez', '2026-03-10 17:24:46'),
+('HormigaOtaku', '$2b$12$8whMB/ILAnt8dMWx0uZ5vOm3dE7E5o/EY8Kz67r689Rpk1NyNofqm', 'Armando Gonzalez', '2026-03-10 17:25:41'),
+('Paco14', '$2b$12$EMpudwJVdCdHgcBVhaz0aOFWUQwTnH9HNi/6bz6YA/YpI48WmIP6u', 'Francisco Arreola', '2026-03-10 17:22:13');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `videojuegos`
+-- Estructura de tabla para la tabla `videojuegos`
 --
 
 CREATE TABLE `videojuegos` (
@@ -165,111 +164,111 @@ CREATE TABLE `videojuegos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
 --
--- Dumping data for table `videojuegos`
+-- Volcado de datos para la tabla `videojuegos`
 --
 
 INSERT INTO `videojuegos` (`id`, `nombre`, `id_tipo`, `imagen`, `created_at`) VALUES
 (1, 'Minecraft', 2, 'https://store-images.s-microsoft.com/image/apps.58378.13850085746326678.826cc014-d610-46af-bdb3-c5c96be4d22c.64287a91-c69e-4723-bb61-03fecd348c2a?q=90&w=480&h=270', '2026-03-05 16:01:52'),
 (2, 'Gears of war', 1, 'https://upload.wikimedia.org/wikipedia/en/thumb/8/82/Gears_of_war_cover_art.jpg/250px-Gears_of_war_cover_art.jpg', '2026-03-05 16:01:52'),
-(3, '\'); Select * from videojuegos;--', 4, 'https://upload.wikimedia.org/wikipedia/en/5/52/Assassin%27s_Creed.jpg', '2026-03-05 16:27:53');
+(3, 'Assassin´s Creed', 4, 'https://upload.wikimedia.org/wikipedia/en/5/52/Assassin%27s_Creed.jpg', '2026-03-05 16:27:53');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `otorga`
+-- Indices de la tabla `otorga`
 --
 ALTER TABLE `otorga`
   ADD PRIMARY KEY (`id_rol`,`id_privilegio`),
   ADD KEY `id_privilegio` (`id_privilegio`);
 
 --
--- Indexes for table `privilegios`
+-- Indices de la tabla `privilegios`
 --
 ALTER TABLE `privilegios`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `roles`
+-- Indices de la tabla `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `tiene`
+-- Indices de la tabla `tiene`
 --
 ALTER TABLE `tiene`
   ADD PRIMARY KEY (`id_rol`,`id_usuario`),
   ADD KEY `id_usuario` (`id_usuario`);
 
 --
--- Indexes for table `tipo`
+-- Indices de la tabla `tipo`
 --
 ALTER TABLE `tipo`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indices de la tabla `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`);
 
 --
--- Indexes for table `videojuegos`
+-- Indices de la tabla `videojuegos`
 --
 ALTER TABLE `videojuegos`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_tipo` (`id_tipo`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `privilegios`
+-- AUTO_INCREMENT de la tabla `privilegios`
 --
 ALTER TABLE `privilegios`
- MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `roles`
+-- AUTO_INCREMENT de la tabla `roles`
 --
 ALTER TABLE `roles`
-   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `tipo`
+-- AUTO_INCREMENT de la tabla `tipo`
 --
 ALTER TABLE `tipo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `videojuegos`
+-- AUTO_INCREMENT de la tabla `videojuegos`
 --
 ALTER TABLE `videojuegos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- Constraints for dumped tables
+-- Restricciones para tablas volcadas
 --
 
 --
--- Constraints for table `otorga`
+-- Filtros para la tabla `otorga`
 --
 ALTER TABLE `otorga`
   ADD CONSTRAINT `otorga_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`),
   ADD CONSTRAINT `otorga_ibfk_2` FOREIGN KEY (`id_privilegio`) REFERENCES `privilegios` (`id`);
 
 --
--- Constraints for table `tiene`
+-- Filtros para la tabla `tiene`
 --
 ALTER TABLE `tiene`
   ADD CONSTRAINT `tiene_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`username`),
   ADD CONSTRAINT `tiene_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`);
 
 --
--- Constraints for table `videojuegos`
+-- Filtros para la tabla `videojuegos`
 --
 ALTER TABLE `videojuegos`
   ADD CONSTRAINT `videojuegos_ibfk_1` FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id`);
