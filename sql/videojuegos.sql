84674f17f427c06c34dbebbdebe9873035c4dbfd
pacoa1 <a01706242@tec.mx>
2026-03-10 11:27:20 -0600

    actualizar script de bd

--- sql/videojuegos.sql
+++ sql/videojuegos.sql
@@ -33,6 +33,16 @@ CREATE TABLE `otorga` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
+--
+-- Dumping data for table `otorga`
+--
+
+INSERT INTO `otorga` (`id_rol`, `id_privilegio`, `created_at`) VALUES
+(1, 1, '2026-03-10 15:47:05'),
+(1, 2, '2026-03-10 15:47:05'),
+(1, 3, '2026-03-10 16:30:05'),
+(2, 1, '2026-03-10 15:47:16');
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +54,15 @@ CREATE TABLE `privilegios` (
   `privilegio` varchar(100) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Dumping data for table `privilegios`
+--
+
+INSERT INTO `privilegios` (`id`, `privilegio`, `created_at`) VALUES
+(1, 'ver_videojuegos', '2026-03-10 15:45:56'),
+(2, 'crear_videojuegos', '2026-03-10 15:45:56'),
+(3, 'actualizar_videojuegos', '2026-03-10 16:29:41');
 
 -- --------------------------------------------------------
 
@@ -57,6 +76,14 @@ CREATE TABLE `roles` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `rol`, `created_at`) VALUES
+(1, 'administrador', '2026-03-10 15:45:23'),
+(2, 'usuario', '2026-03-10 15:45:23');
+
 -- --------------------------------------------------------
 
 --
@@ -68,6 +95,37 @@ CREATE TABLE `tiene` (
   `id_rol` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Dumping data for table `tiene`
+--
+
+INSERT INTO `tiene` (`id_usuario`, `id_rol`, `created_at`) VALUES
+('citibanamex', 1, '2026-03-10 15:46:32'),
+('acantu', 2, '2026-03-10 15:46:32'),
+('sebas', 2, '2026-03-10 16:30:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipo`
+--
+
+CREATE TABLE `tipo` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
+
+--
+-- Dumping data for table `tipo`
+--
+
+INSERT INTO `tipo` (`id`, `nombre`, `created_at`) VALUES
+(1, 'shooter', '2026-03-10 16:25:12'),
+(2, 'sandbox', '2026-03-10 16:25:12'),
+(3, 'acción', '2026-03-10 16:25:56'),
+(4, 'plataformas', '2026-03-10 16:25:56');
 
 -- --------------------------------------------------------
 
@@ -89,7 +147,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`username`, `password`, `nombre`, `created_at`) VALUES
 ('acantu', '$2b$12$rXegq4954JwPGwLuJ5uiR.K/Y/JtwX0pXJR7y/HtzqBFutGJgXp9W', 'Andrea Cantú', '2026-03-09 15:54:58'),
 ('citibanamex', '$2b$12$0TO6GxusFKTEwZ4cGqnd5.RWLI.2UK5J3ezfChZ0Rfnqzqg4NCLA.', 'Alexis Berthou', '2026-03-09 15:58:01'),
-('lalo3', '$2b$12$9BaWo0uW13UhhzR0qLpjQuR0eE6YojdHLsLGFfjPrGVTWdbIyUp9y', 'asdfg', '2026-03-09 16:38:39');
+('lalo3', '$2b$12$9BaWo0uW13UhhzR0qLpjQuR0eE6YojdHLsLGFfjPrGVTWdbIyUp9y', 'asdfg', '2026-03-09 16:38:39'),
+('sebas', '$2b$12$V1BGWjk1TWtbMON46n2kkOyamsST//NqHTFBh9YX/ny7me6ZRBK2i', 'Sebastián Mansilla', '2026-03-10 16:06:55');
 
 -- --------------------------------------------------------
 
@@ -100,6 +159,7 @@ INSERT INTO `users` (`username`, `password`, `nombre`, `created_at`) VALUES
 CREATE TABLE `videojuegos` (
   `id` int(11) NOT NULL,
   `nombre` varchar(250) NOT NULL,
+  `id_tipo` int(11) NOT NULL,
   `imagen` varchar(300) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
@@ -108,10 +168,10 @@ CREATE TABLE `videojuegos` (
 -- Dumping data for table `videojuegos`
 --
 
-INSERT INTO `videojuegos` (`id`, `nombre`, `imagen`, `created_at`) VALUES
-(1, 'Minecraft', 'https://store-images.s-microsoft.com/image/apps.58378.13850085746326678.826cc014-d610-46af-bdb3-c5c96be4d22c.64287a91-c69e-4723-bb61-03fecd348c2a?q=90&w=480&h=270', '2026-03-05 16:01:52'),
-(2, 'Gears of war', 'https://upload.wikimedia.org/wikipedia/en/thumb/8/82/Gears_of_war_cover_art.jpg/250px-Gears_of_war_cover_art.jpg', '2026-03-05 16:01:52'),
-(3, '\'); Select * from videojuegos;--', 'https://upload.wikimedia.org/wikipedia/en/5/52/Assassin%27s_Creed.jpg', '2026-03-05 16:27:53');
+INSERT INTO `videojuegos` (`id`, `nombre`, `id_tipo`, `imagen`, `created_at`) VALUES
+(1, 'Minecraft', 2, 'https://store-images.s-microsoft.com/image/apps.58378.13850085746326678.826cc014-d610-46af-bdb3-c5c96be4d22c.64287a91-c69e-4723-bb61-03fecd348c2a?q=90&w=480&h=270', '2026-03-05 16:01:52'),
+(2, 'Gears of war', 1, 'https://upload.wikimedia.org/wikipedia/en/thumb/8/82/Gears_of_war_cover_art.jpg/250px-Gears_of_war_cover_art.jpg', '2026-03-05 16:01:52'),
+(3, '\'); Select * from videojuegos;--', 4, 'https://upload.wikimedia.org/wikipedia/en/5/52/Assassin%27s_Creed.jpg', '2026-03-05 16:27:53');
 
 --
 -- Indexes for dumped tables
@@ -144,6 +204,12 @@ ALTER TABLE `tiene`
   ADD KEY `id_usuario` (`id_usuario`);
 
 --
+-- Indexes for table `tipo`
+--
+ALTER TABLE `tipo`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -153,7 +219,8 @@ ALTER TABLE `users`
 -- Indexes for table `videojuegos`
 --
 ALTER TABLE `videojuegos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_tipo` (`id_tipo`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -163,13 +230,19 @@ ALTER TABLE `videojuegos`
 -- AUTO_INCREMENT for table `privilegios`
 --
 ALTER TABLE `privilegios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tipo`
+--
+ALTER TABLE `tipo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `videojuegos`
@@ -194,6 +267,12 @@ ALTER TABLE `otorga`
 ALTER TABLE `tiene`
   ADD CONSTRAINT `tiene_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `users` (`username`),
   ADD CONSTRAINT `tiene_ibfk_2` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`);
+
+--
+-- Constraints for table `videojuegos`
+--
+ALTER TABLE `videojuegos`
+  ADD CONSTRAINT `videojuegos_ibfk_1` FOREIGN KEY (`id_tipo`) REFERENCES `tipo` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
