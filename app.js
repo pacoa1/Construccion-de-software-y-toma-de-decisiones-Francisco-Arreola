f9cbb429119276cc16b99362b8a936add63fe0a1
pacoa1 <a01706242@tec.mx>
2026-03-23 09:41:35 -0600

    instalar y configurar multer

--- app.js
+++ app.js
@@ -17,6 +17,27 @@ app.use(session({
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
+const multer = require('multer');
+
+//fileStorage: Es nuestra constante de configuración para manejar el almacenamiento
+const fileStorage = multer.diskStorage({
+    destination: (request, file, callback) => {
+        //'uploads': Es el directorio del servidor donde se subirán los archivos 
+        callback(null, 'public/uploads');
+    },
+    filename: (request, file, callback) => {
+        //aquí configuramos el nombre que queremos que tenga el archivo en el servidor, 
+        //para que no haya problema si se suben 2 archivos con el mismo nombre concatenamos el timestamp
+        callback(null, new Date().toString() + '-' + file.originalname);
+    },
+});
+
+//En el registro, pasamos la constante de configuración y
+//usamos single porque es un sólo archivo el que vamos a subir, 
+//pero hay diferentes opciones si se quieren subir varios archivos. 
+//'archivo' es el nombre del input tipo file de la forma
+app.use(multer({ storage: fileStorage }).single('imagen')); 
+
 const csrf = require('csurf');
 const csrfProtection = csrf();
 app.use(csrfProtection);
